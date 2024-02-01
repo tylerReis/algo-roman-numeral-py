6692943b180e03854a5d394e1651ba3bee9dcaad
tylerReis <tyler.reis.j@outlook.com>
2024-02-01 16:38:45 -0500

    initial solution

--- roman_numerals.py
+++ roman_numerals.py
@@ -1,3 +1,26 @@
 def to_roman(num):
-    # write your code here!
-    pass
+    output = ""
+    roman_numerals = {
+    'M' : 1000,
+    'CM': 900,
+    'D' : 500,
+    'CD': 400,
+    'C' : 100,
+    'L' : 50,
+    'XL': 40,
+    'X' : 10,
+    'IX': 9,
+    'V' : 5,
+    'IV': 4,
+    'I' : 1,
+    }
+    
+    for k in roman_numerals:
+        while num >= roman_numerals[k]:
+            output = output + k
+            num = num - roman_numerals[k]
+    return output
+
+print(to_roman(944))
+
+     
